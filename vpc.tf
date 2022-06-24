039e5c03a919d497c6b7f2238ee9fc3555d52710
Gary Nation <gary_nation@hotmail.com>
2022-06-24 15:55:15 +0100

    new VPC

--- vpc.tf
+++ vpc.tf
@@ -13,21 +13,28 @@ resource "aws_vpc" "aws-demo2" {
   }
 }
 resource "aws_vpc" "aws-demo3" {
-  cidr_block = "10.1.1.1/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "${var.name}-aws-demo3"
   }
 }
 resource "aws_vpc" "aws-demo4" {
-  cidr_block = "10.1.1.1/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "${var.name}-aws-demo3"
   }
 }
 resource "aws_vpc" "aws-demo5" {
-  cidr_block = "10.1.1.1/16"
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.name}-aws-demo3"
+  }
+}
+resource "aws_vpc" "just-eat" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "${var.name}-aws-demo3"
