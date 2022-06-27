2bad14f1723bb94cc970eb0bdf7354f3876e0afd
Gary Nation <gary_nation@hotmail.com>
2022-06-27 12:22:59 +0100

    new VPC

--- vpc.tf
+++ vpc.tf
@@ -33,3 +33,10 @@ resource "aws_vpc" "aws-demo5" {
     Name = "${var.name}-aws-demo3"
   }
 }
+resource "aws_vpc" "aws-demo6" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.name}-aws-demo3"
+  }
+}
